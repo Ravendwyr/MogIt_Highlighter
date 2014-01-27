e5719d112cdafbf8790f994e8094e2f413049d5a
Ethan Centaurai <ethancentaurai@outlook.com>
2014-01-27 14:30:43 +0000

    Highlight vendor goods that are in your MogIt Wishlist.

--- Core.lua
+++ Core.lua
@@ -35,4 +35,54 @@ local function HighlightQuestRewards()
 	end
 end
 
+
+local function HighlightMerchantGoods()
+	for i=1, MERCHANT_ITEMS_PER_PAGE do
+		ActionButton_HideOverlayGlow( _G["MerchantItem" .. i .. "ItemButton"] )
+	end
+
+	ActionButton_HideOverlayGlow( _G["MerchantBuyBackItemItemButton"] )
+
+	if MerchantFrame:IsShown() then
+		if MerchantFrame.selectedTab == 1 then
+			for i=1, MERCHANT_ITEMS_PER_PAGE do
+				local index = ( ((MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE) + i )
+				local itemLink = GetMerchantItemLink(index)
+
+				if itemLink then
+					local itemID = tonumber(itemLink:match("item:(%d+)"))
+
+					if Wishlist:IsItemInWishlist(itemID) then
+						ActionButton_ShowOverlayGlow( _G["MerchantItem" .. i .. "ItemButton"] )
+					end
+				end
+			end
+
+			local buyBackLink = GetBuybackItemLink(GetNumBuybackItems())
+
+			if itemLink then
+				local itemID = tonumber(itemLink:match("item:(%d+)"))
+
+				if Wishlist:IsItemInWishlist(itemID) then
+					ActionButton_ShowOverlayGlow( _G["MerchantBuyBackItemItemButton"] )
+				end
+			end
+		else
+			for i=1, BUYBACK_ITEMS_PER_PAGE do
+				local itemLink = GetBuybackItemLink(i)
+
+				if itemLink then
+					local itemID = tonumber(itemLink:match("item:(%d+)"))
+
+					if Wishlist:IsItemInWishlist(itemID) then
+						ActionButton_ShowOverlayGlow( _G["MerchantItem" .. i .. "ItemButton"] )
+					end
+				end
+			end
+		end
+	end
+end
+
+
 hooksecurefunc("QuestInfo_Display", HighlightQuestRewards)
+hooksecurefunc('MerchantFrame_Update', HighlightMerchantGoods)
